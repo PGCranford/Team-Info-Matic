d3edd0082f6a10307369903d2b3b92a4f0449444
PGCranford <PGCranford@gmail.com>
2022-07-08 12:54:42 -0400

    filefolder set up

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,12 @@
+const { default: test } = require('node:test');
+const Employee = require('../lib/Employee.js');
+
+test('creates Employee object', () => {
+
+    const employee = new employee('Ned', 64, 'ned@nedcentral.com', 8);
+
+    expect(role(employee)).toBe('object')
+
+
+
+})
